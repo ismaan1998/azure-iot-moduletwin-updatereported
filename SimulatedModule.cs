2e54bebd20001cc71b49b33123c00560fae0a41d
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2023-03-12 15:31:47 +0530

    added reportedpropertiesupdation code

---
+++ SimulatedModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using System.Threading.Tasks;
+
+namespace simulatedDevice
+{
+
+    
+    class SimulatedDevice
+    {
+        static string ModuleConnectionString = "module twin connection string";
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Connecting to hub");
+                ModuleClient Client = ModuleClient.CreateFromConnectionString(ModuleConnectionString,
+                    TransportType.Mqtt);
+                Console.WriteLine("Retrieving twin");
+                var twin = await Client.GetTwinAsync(); // this method is not supported in .net 6.0 use earlier version. 
+               
+
+                var reportedProperties = new TwinCollection();
+                var tempControl = new TwinCollection();
+                tempControl["minTemp"] = "-10";
+                tempControl["maxTemp"] = "70";
+                reportedProperties["tempControl"] = tempControl;
+
+                await Client.UpdateReportedPropertiesAsync(reportedProperties);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Error in sample: {0}", ex.Message);
+            }
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
